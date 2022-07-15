d98a5922f2a35c4d4f094786e07c247a2b743525
brenno-araujo <brennoaraujo@yahoo.com>
2022-07-15 12:00:33 -0300

    config init

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3336);
+app.listen(3336, () => {
+  console.log('Server started on port 3336 🚀');
+});
