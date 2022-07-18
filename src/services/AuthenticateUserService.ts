cb3ad1a0c1a8aba17e7e2797a9f45949aee2f0cc
brenno-araujo <brennoaraujo@yahoo.com>
2022-07-18 11:10:08 -0300

    refactor: autehnticate

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,7 +29,15 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination');
     }
 
-    return { user };
+    const token = sign({}, '443199c609fd3dd2d04c3f834d3685aa', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
